758e6c0a0469ceab40c2ee417426237e2bcf1465
Wayne Jessen <wjessen@thefarmersdog.com>
2024-05-24 11:30:40 -0700

    added lsp

--- init.lua
+++ init.lua
@@ -19,20 +19,33 @@
 -- 18. org mode(?)
 -- 19. make all quotes single quotes (part of linting)
 -- 20. split one line into multi lines
+-- 21. auto number lists
+-- 22. search for symbol 
 require "paq" {
-    "savq/paq-nvim", -- Let Paq manage itself
+	"savq/paq-nvim", -- Let Paq manage itself
 	"nvim-lua/plenary.nvim",
 	"nvim-telescope/telescope.nvim",
-    "ibhagwan/fzf-lua",	
-    "rebelot/kanagawa.nvim",
-    {"nvim-treesitter/nvim-treesitter", build = ':TSUpdate'},
+    	"ibhagwan/fzf-lua",
+    	"rebelot/kanagawa.nvim",
+    	{"nvim-treesitter/nvim-treesitter", build = ':TSUpdate'},
+    	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'};
+    	{'williamboman/mason.nvim'};
+    	{'williamboman/mason-lspconfig.nvim'};
+	{'neovim/nvim-lspconfig'};
+	{'hrsh7th/nvim-cmp'};
+	{'hrsh7th/cmp-nvim-lsp'};
+	{'L3MON4D3/LuaSnip'};
 }
 -- remaps
 vim.cmd [[set mouse=a]]
 
 vim.g.mapleader = " "
-
+	
 vim.wo.number = true
+
+-- dir view
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 -- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -58,3 +71,44 @@ require('nvim-treesitter.configs').setup({
 	ensure_installed = {
 		'c', 'lua', 'typescript', 'javascript', 'python', 'go', 'sql', 'terraform', 'hcl', 'json'}
 	})
+
+-- lsp setup
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+lsp_zero.default_keymaps({buffer = bufnr}) end)
+lsp_zero.setup_nvim_cmp({
+	select_behavior = 'insert'
+})
+
+
+-- to learn how to use mason.nvim
+-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+require('mason').setup({})
+
+require('mason-lspconfig').setup({
+  ensure_installed = {'tsserver', 'terraformls', 'clangd', 'lua_ls'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
+
+
+--completions
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
