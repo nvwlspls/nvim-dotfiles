de0a2a7e5a789b8e5a347ef3e469abdc77ded87c
Wayne Jessen <wjessen@thefarmersdog.com>
2024-05-24 13:41:54 -0700

    added harpoon

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@
 --x 5. color scheme
 --x 6. better syntax highliting (treesitter)
 --x 7. lsp
--- 8. copilot
+--x 8. copilot
 -- 9. better indenting
 -- 10. git integration(?)
 -- 11. debugger
@@ -20,28 +20,33 @@
 -- 19. make all quotes single quotes (part of linting)
 -- 20. split one line into multi lines
 -- 21. auto number lists
--- 22. search for symbol 
--- 23. switch buffers (harpoon+)
+-- 22. search for symbol
+--x 23. switch buffers (harpoon+)
 require "paq" {
 	"savq/paq-nvim", -- Let Paq manage itself
 	"nvim-lua/plenary.nvim",
 	"nvim-telescope/telescope.nvim",
-    	"ibhagwan/fzf-lua",
-    	"rebelot/kanagawa.nvim",
-    	{"nvim-treesitter/nvim-treesitter", build = ':TSUpdate'},
-    	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'};
-    	{'williamboman/mason.nvim'};
-    	{'williamboman/mason-lspconfig.nvim'};
-	{'neovim/nvim-lspconfig'};
-	{'hrsh7th/nvim-cmp'};
-	{'hrsh7th/cmp-nvim-lsp'};
-	{'L3MON4D3/LuaSnip'};
+	"ibhagwan/fzf-lua",
+	"rebelot/kanagawa.nvim",
+	{ "nvim-treesitter/nvim-treesitter",  build = ':TSUpdate' },
+	{ 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+	{ 'williamboman/mason.nvim' },
+	{ 'williamboman/mason-lspconfig.nvim' },
+	{ 'neovim/nvim-lspconfig' },
+	{ 'hrsh7th/nvim-cmp' },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'L3MON4D3/LuaSnip' },
+	'github/copilot.vim',
+	'burntSushi/ripgrep',
+	{'junegunn/fzf', build = ':fzf#install()'},
+	{'junegunn/fzf.vim'},
+	'nvim-tree/nvim-web-devicons',
+	'ThePrimeagen/harpoon',
 }
 -- remaps
 vim.cmd [[set mouse=a]]
 
 vim.g.mapleader = " "
-
 vim.wo.number = true
 
 -- dir view
@@ -53,10 +58,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
--- 'command pallet'
-local fzf = require('fzf-lua')
-vim.keymap.set('n', '<S-p>', fzf.commands,{})
+vim.keymap.set('n', '<S-p>', builtin.commands, {}) -- 'command pallet'
 
 
 -- kanagawa color scheme
@@ -70,43 +72,62 @@ vim.cmd("colorscheme kanagawa")
 -- treesitter languages
 require('nvim-treesitter.configs').setup({
 	ensure_installed = {
-		'c', 'lua', 'typescript', 'javascript', 'python', 'go', 'sql', 'terraform', 'hcl', 'json'}
-	})
+		'c', 'lua', 'typescript', 'javascript', 'python', 'go', 'sql', 'terraform', 'hcl', 'json' }
+})
 
 -- lsp setup
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-lsp_zero.default_keymaps({buffer = bufnr}) end)
-lsp_zero.setup_nvim_cmp({
-	select_behavior = 'insert'
-})
-
- -- to learn how to use mason.nvim read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md require('mason').setup({})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
+end)
+--mason for lsp
 require('mason').setup({})
-
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'terraformls', 'clangd', 'lua_ls'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+	ensure_installed = { 'tsserver', 'terraformls', 'clangd', 'lua_ls' },
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+	},
 })
 
 --completions
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp_action.luasnip_supertab(),
-    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+	mapping = cmp.mapping.preset.insert({
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
 })
+
+
+-- copilot <use ctl + j to accept>
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+
+-- devicons
+require('nvim-web-devicons').setup({})
+
+-- harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set('n', '<leader>ha', mark.add_file)
+vim.keymap.set('n', '<leader>hq', ui.toggle_quick_menu)
+
+vim.keymap.set('n', '<leader>hw', function() ui.nav_file(1) end)
+vim.keymap.set('n', '<leader>he', function() ui.nav_file(2) end)
+vim.keymap.set('n', '<leader>hr', function() ui.nav_file(3) end)
+vim.keymap.set('n', '<leader>ht', function() ui.nav_file(4) end)
